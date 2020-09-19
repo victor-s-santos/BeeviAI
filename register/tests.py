07ea297a7fbec9397a575818b974eef508b47a0e
victor-s-santos <victor.s.silva@unesp.br>
2020-09-19 15:08:34 -0300

    tests in register app has been done

--- register/tests.py
+++ register/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .forms import RegisterForm
 
-# Create your tests here.
+class RegisterTest(TestCase):
+    
+    def setUp(self):
+        self.response = self.client.get('/register/')
+
+    def test_get(self):
+        """Must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use register/register.html"""
+        self.assertTemplateUsed(self.response, 'register/register.html')
+
+    def test_html(self):
+        """Template must contain input tags"""
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="submit"', 1)
+
+    def test_csrf(self):
+        """Template must contain csrf_token"""
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """Context must have register form"""
+        form = self.response.context['form']
+        self.assertIsInstance(form, RegisterForm)
+
+    def test_form_has_fields(self):
+        """Form must have 4 fields"""
+        form = self.response.context['form']
+        self.assertSequenceEqual(['username', 'email', 'password1', 'password2'], list(form.fields))
