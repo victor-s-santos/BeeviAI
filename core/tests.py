9de4851da05934a1ff1104a3fcfc6394f2941f05
victor-s-santos <victor.s.silva@unesp.br>
2020-09-19 14:20:37 -0300

    template test

--- core/tests.py
+++ core/tests.py
@@ -6,3 +6,8 @@ class HomeTest(TestCase):
         """Must returns status code 200"""
         response = self.client.get('/')
         self.assertEqual(200, response.status_code)
+
+    def test_template(self):
+        """Must uses index.html"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
